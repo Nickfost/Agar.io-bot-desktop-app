7206be8de18d06b92a12d41a15a8137a78b42b5b
FliiFe <theophile.cailliau@gmail.com>
2015-08-30 21:37:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+### Description
+
+
+This is an app built on `electron`, which uses web technologies on top of `nodejs`.
+
+### Installation
+
+A few step are required to run this app.
+
+***Firstly, clone this repo with `git clone`, or download it with the "Download as zip" button***
+
+Currently, I couldn't find a way to pack everything in one file, so help is welcome.
+
+Howerver, you only have to launch one file :
+
+* On Windows :
+    + Double click on `start.bat`
+    + Inside the app directory, run the command `start.bat`
+* On Mac OSX (**_To be confirmed_**) and Linux :
+    + Double click on `start.sh`
+    + Inside the app directory, run the command : `./start.sh`
+
+### Advanced Installation
+
+This is if you want to contribute or if you like to understand anything.
+
+* On Windows, Linux, and Mac OSX:
+    + Install `nodejs`
+    + Then, in a comand prompt, run :
+          npm install -g express
+          npm install -g electron
+          electron path_to_app_directory
+
+### Troubleshooting
+
+If you are having issues, please refer to this section before posting an issue
+
+* **'electron' is not recognised as internal command'**
+    + Please check that `electron` is in your %PATH% variable.
+    + Download `electron` from the [official repository](https://github.com/atom/electron/releases).
+
+    **_Make sure to select the right version!_** (`electron-v*-win32-ia32.zip` or `electron-v*-win64-x64.zip`)
+
+    Then extract it somewhere, and add the path to your %PATH% environment variable.
---
+++ main.js
@@ -0,0 +1,99 @@
+var app = require('app'); // Module to control application life.
+var express = require('express');
+var exapp = express();
+var fs = require('fs');
+var launcherCode = '';
+var BrowserWindow = require('browser-window'); // Module to create native browser window.
+
+// Report crashes to our server.
+require('crash-reporter').start();
+
+function writeLauncher(codeToWrite) {
+    fs.writeFile(__dirname + '/public/launcher.user.js', codeToWrite, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    });
+}
+
+function readLauncher() {
+    fs = require('fs')
+    fs.readFile(__dirname + '/public/launcher.user.js', 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        launcherCode = data;
+    });
+}
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is GCed.
+
+exapp.use(express.static('public'));
+
+exapp.listen(3000);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
+var ipc = require('ipc');
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', function () {
+    var agario = createBrowser('http://agar.io/', false, 800, 600);
+
+    agario.setSkipTaskbar(true);
+    agario.minimize();
+    readLauncher();
+
+    var local = createBrowser('http://localhost:3000/index.html', true, 800, 600);
+
+    ipc.on('showagario', function (event, arg) {
+        console.log('Showing agario'); // prints "ping"
+        writeLauncher(launcherCode.replace('names = ["NotReallyABot"],', 'names = ["' + arg + '"],'));
+        agario.restore();
+        agario.setSkipTaskbar(false);
+        setTimeout(function() {
+            loadBot(agario);
+        }, 1000);
+        local.close();
+    });
+
+    function loadBot(win) {
+        win.webContents.executeJavaScript("var head=document.getElementsByTagName('head')[0],script=document.createElement('script');script.type='text/javascript',script.src='http://localhost:3000/launcher.user.js',head.appendChild(script);")
+        win.webContents.executeJavaScript("var head=document.getElementsByTagName('head')[0],script=document.createElement('script');script.type='text/javascript',script.src='http://localhost:3000/bot.user.js',head.appendChild(script);")
+    }
+
+    function createBrowser(url, nodeIntegration, w, h) {
+        var mainWindow = null;
+        // Create the browser window.
+        mainWindow = new BrowserWindow({
+            width: w,
+            height: h,
+            "node-integration": nodeIntegration
+        });
+
+        // and load the index.html of the app.
+        mainWindow.loadUrl(url);
+
+
+        // Open the devtools.
+        //mainWindow.openDevTools();
+
+        // Emitted when the window is closed.
+        mainWindow.on('closed', function () {
+            // Dereference the window object, usually you would store windows
+            // in an array if your app supports multi windows, this is the time
+            // when you should delete the corresponding element.
+            mainWindow = null;
+        });
+        return mainWindow;
+    }
+});
